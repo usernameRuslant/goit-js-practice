2f8969d10e52d21a183560ccf047dd096dacee0f
Ruslan Tukhfyatullov <ruslan.tukhfyatullov@gmail.com>
2025-06-17 18:36:47 +0300

    task

--- js/test.js
+++ js/test.js
@@ -59,3 +59,149 @@
 // console.log(checkStorage(200, 20));
 // console.log(checkStorage(200, 150));
 // console.log(checkStorage(150, 180));
+// Функція checkStorage(available, ordered) перевіряє можливість оформлення замовлення
+// і повертає повідомлення про результат. Вона оголошує два параметри, значення яких будуть задаватися під час її виклику.
+
+// available - доступна кількість товарів на складі
+// ordered - кількість одиниць товару в замовленні
+// Використовуючи розгалуження, доповни код функції таким чином, щоб:
+
+// Якщо в замовленні ще немає товарів, тобто значення параметра ordered дорівнює 0, функція повертає рядок "There are no products in the order!".
+// Якщо товарів у замовленні більше, ніж їх доступно на складі, то функція повертає рядок "Your order is too large, there are not enough items in stock!".
+// В іншому випадку функція повертає рядок "The order is accepted, our manager will contact you".
+// function checkStorage(available, ordered) {
+//   if (ordered === 0) {
+//     return 'There are no products in the order!';
+//   } else if (ordered > available) {
+//     return 'Your order is too large, there are not enough items in stock!';
+//   } else {
+//     return 'The order is accepted, our manager will contact you';
+//   }
+// }
+
+// console.log(checkStorage(100, 50));
+// console.log(checkStorage(100, 130));
+// console.log(checkStorage(70, 0));
+// console.log(checkStorage(200, 20));
+// console.log(checkStorage(200, 250));
+// console.log(checkStorage(150, 0));
+// console.log(checkStorage(80, 80));
+// ============================================================================================================
+// Функція checkPassword(password) порівнює переданий їй пароль (параметр password) зі збереженим паролем адміністратора (correctPassword) і повертає рядок з повідомленням про результат.
+
+// Використовуючи тернарний оператор, доповни функцію таким чином, щоб:
+
+// Якщо значення password і correctPassword збігаються, функція повертала рядок "Access granted"
+// В іншому випадку, функція повертала рядок "Access denied, wrong password!"
+
+// function checkPassword(password) {
+//   const correctPassword = 'jqueryismyjam';
+//   return password === correctPassword
+//     ? 'Access granted'
+//     : 'Access denied, wrong password!';
+// }
+// console.log(checkPassword('jqueryismyjam'));
+// console.log(checkPassword('angul4r1sl1f3'));
+// console.log(checkPassword('r3actsux'));
+// ============================================================================================================
+// Функція getSubscriptionPrice(type) отримує рядок з типом передплати користувача (параметр type), перевіряє її на збіг з трьома можливими типами щомісячної передплати та повертає ціну, або повідомлення про неправильний тип передплати.
+
+// Якщо значення параметра type — це рядок:
+
+// "starter" — ціна передплати 0 кредитів.
+// "professional" — ціна передплати 20 кредитів.
+// "organization" — ціна передплати 50 кредитів.
+// Для будь-якого іншого значення параметра type повертається рядок "Invalid subscription type!".
+
+// Використай оператор switch для розв'язання цієї задачі!
+// function getSubscriptionPrice(type) {
+//   let price;
+//   switch (type) {
+//     case 'starter':
+//       price = 0;
+//       break;
+//     case 'professional':
+//       price = 20;
+//       break;
+//     case 'organization':
+//       price = 50;
+//       break;
+//     default:
+//       return 'Invalid subscription type!';
+//   }
+//   return price;
+// }
+
+// console.log(getSubscriptionPrice('professional'));
+// console.log(getSubscriptionPrice('starter'));
+// console.log(getSubscriptionPrice('organization'));
+// console.log(getSubscriptionPrice('premium'));
+// Функція isNumberInRange(start, end, number) перевіряє, чи входить число у проміжок. Вона оголошує три параметри, значення яких будуть задаватися під час її виклику:
+
+// number - число, входження якого перевіряється
+// start - початок числового проміжку
+// end - кінець числового проміжку
+// Використовуючи оператор &&, доповни функцію isNumberInRange таким чином, щоб вона повертала результат входження number у числовий проміжок від start до end включно. Тобто число повинно бути одночасно і більшим або дорівнювати start, і меншим або дорівнювати end. Результатом виклику функції має бути буль true або false.
+
+// function isNumberInRange(start, end, number) {
+//   if (number > start && number < end) {
+//     return 'true';
+//   } else {
+//     return 'false';
+//   }
+// }
+
+// console.log(isNumberInRange(10, 30, 17));
+// console.log(isNumberInRange(10, 30, 5));
+// console.log(isNumberInRange(20, 50, 24));
+// console.log(isNumberInRange(20, 50, 76));
+
+// Функція checkAccess(subType) перевіряє, чи може користувач отримати доступ до контенту. Перевірка відбувається за типом передплати. Використовуючи оператор "АБО", доповни код функції так, щоб якщо значення параметра subType дорівнює рядкам "pro" або "vip", то функція повертала true і користувач отримував доступ. В іншому випадку повертала false.
+// function checkAccess(subType) {
+//   if (subType === 'pro' || 0) {
+//     return true;
+//   } else if (subType === 'vip' || 0) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// console.log(checkAccess('pro'));
+// console.log(checkAccess('starter'));
+// console.log(checkAccess('vip'));
+// console.log(checkAccess('free'));
+
+// Функція toggleModalVisibility(isVisible) перемикає стан відображення модального вікна. У параметрі isVisible вона очікує буль true або false. Доповни код функції так, щоб вона повертала протилежне значення цього параметра.
+// function toggleModalVisibility(isVisible) {
+//   if (isVisible === true) {
+//     return !true;
+//   } else {
+//     return !false;
+//   }
+// }
+
+// console.log(toggleModalVisibility(true));
+// console.log(toggleModalVisibility(false));
+// Функція getSubstring(string, length) приймає рядок і повертає новий підрядок (його часткову копію). Вона оголошує два параметри, значення яких будуть задаватися під час її виклику:
+
+// string - оригінальний рядок
+// length - довжина нового підрядка
+// Доповни код функції так, щоб вона повертала новий підрядок, який починається від початку string і має довжину length.
+// function getSubstring(string, length) {
+//   return string.slice(0, length);
+// }
+
+// console.log(getSubstring('Hello world', 3));
+// console.log(getSubstring('Hello world', 5));
+// console.log(getSubstring('Hello world', 8));
+// console.log(getSubstring('Hello world', 11));
+// console.log(getSubstring('Hello world', 0));
+// Функція normalizeInput(input, to) оголошує два параметри:
+
+// input - рядок, який потрібно привести до певного регістру
+// to - рядок с двома можливими значеннями: "upper" або "lower", що вказують до якого регістру потрібно привести значення input
+// Доповни код функції таким чином, щоб:
+
+// Якщо значення параметра to — це рядок "upper", то функція повертала копію рядка input, але у верхньому регістрі
+// В іншому разі, функція повертала копію рядка inputу нижньому регістрі
